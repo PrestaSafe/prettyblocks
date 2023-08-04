1b09f03b6a9dbef48c49142505a17ebde658779e
Guillaume Batier <contact@prestasafe.com>
2023-08-04 15:10:15 +0200

    Update prettyblocks.php
    update version

--- prettyblocks.php
+++ prettyblocks.php
@@ -57,7 +57,7 @@ class PrettyBlocks extends Module implements WidgetInterface
     {
         $this->name = 'prettyblocks';
         $this->tab = 'administration';
-        $this->version = '2.1.1';
+        $this->version = '2.2.0';
         $this->author = 'PrestaSafe';
         $this->need_instance = 1;
         $this->js_path = $this->_path . 'views/js/';
