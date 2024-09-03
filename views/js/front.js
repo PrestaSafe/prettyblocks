1044dc3fa88bb58254b9b96017bcdd630ac49148
Prestasafe <contact@prestasafe.com>
2024-09-03 17:42:17 +0200

    update tinyslider configuration

--- views/js/front.js
+++ views/js/front.js
@@ -1,22 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
     if (document.querySelectorAll('.prettyblocks-tns').length > 0) {
         var sliders = document.querySelectorAll('.prettyblocks-tns');
+
         sliders.forEach(function (slider) {
             tns({
                 container: slider,
                 items: 1,
                 slideBy: 'page',
+                autoplay: true,
+                autoplayTimeout: 4000,
+                autoplayButtonOutput: false,
+                nav: false,
+                controls: false,
+                loop: true,
+                gutter: 10,
                 autoWidth: true,
                 center: true,
-                // autoHeight: true,
-                autoplay: true,
-                nav: false, // disable dots
-                controls: false, // disable controls
-                autoplayButtonOutput: false,
-                mouseDrag: true,
-                loop: true,
-                rewind: true,
-                autoplayTimeout: 4000,
+                autoHeight: true,
+                responsive: {
+                    640: {
+                        items: 1
+                    },
+                    768: {
+                        items: 1
+                    },
+                    1024: {
+                        items: 1
+                    }
+                }
             });
         });
     }
