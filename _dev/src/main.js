8649bd68be535242c107dd07d7da4104b542a068
Antoine Araujo <aaraujo@axome.com>
2024-10-15 14:04:37 +0200

    🐛 Forgot main.js

--- _dev/src/main.js
+++ _dev/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import './index.css'
+import { updateEditorAndGetEditorsNumbers } from './scripts/editorManager';
+
 const app = createApp(App).use(createPinia())
 app.mount('#app')
 
